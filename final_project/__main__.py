8b15b822a4078151cac2270342c6e75790375d23
phi1ny3 <38825543+phi1ny3@users.noreply.github.com>
2021-04-06 18:56:38 -0600

    More Adjustments

--- final_project/__main__.py
+++ final_project/__main__.py
@@ -54,7 +54,7 @@ class MyGame(arcade.View):
         # super().__init__(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT, constants.SCREEN_TITLE)
         super().__init__()
         self.master_volume = constants.DEFAULT_VOLUME
-        self.background_music = arcade.Sound(constants.MAIN_SCREEN_SOUND)
+        self.background_music = arcade.Sound(constants.MAIN_MUSIC)
         self.background_music.play(self.master_volume, loop = True)
 
         # These are 'lists' that keep track of our sprites. Each sprite should
--- final_project/fishy/constants.py
+++ final_project/fishy/constants.py
@@ -4,7 +4,7 @@ from pathlib import Path
 home = Path.home()
 assets: Path = Path(__file__).parent / 'assets'
 
-MAIN_SCREEN_SOUND = assets / 'Sappheiros - Lights.mp3'
+MAIN_MUSIC = assets / 'Sappheiros - Lights.mp3'
 DEFAULT_VOLUME = .25
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 1000
